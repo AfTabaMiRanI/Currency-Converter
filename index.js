56b25d9bd7ec92f2ff6d76a6e83712974fed8f75
Mohammad Aftab <160458810+AfTabaMiRanI@users.noreply.github.com>
2024-04-07 13:11:49 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const currency = {
+    USD: 1, // base currency
+    PKR: 280,
+    INR: 74.57,
+    EUR: 0.91,
+    GBP: 0.76
+};
+let userAnswer = await inquirer.prompt([
+    {
+        name: "from",
+        message: chalk.bgBlueBright.bold("Enter From currency"),
+        type: "list",
+        choices: ["USD", "PKR", "INR", "EUR", "GBP"]
+    },
+    {
+        name: "to",
+        message: chalk.bgRedBright.italic("Enter to currency"),
+        type: "list",
+        choices: ["USD", "PKR", "INR", "EUR", "GBP"]
+    },
+    {
+        name: "amount",
+        message: chalk.bgGray("Enter Your Amount"),
+        type: "number"
+    }
+]);
+let fromAmount = currency[userAnswer.from];
+let toAmount = currency[userAnswer.to];
+let amount = userAnswer.amount;
+let baseAmount = amount / fromAmount; // USD base Currency
+let convertedAmount = chalk.whiteBright(Math.round(baseAmount * toAmount));
+console.log(chalk.white(convertedAmount));
